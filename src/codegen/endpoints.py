eb3fb123bc771f11e1fbd0858fe9155a79d54cde
Christine Wang <christine@codegen.com>
2024-12-12 13:09:00 -0800

    chore: add dotenv + remove codemd-id var (#32)

--- src/codegen/endpoints.py
+++ src/codegen/endpoints.py
@@ -1,3 +1,3 @@
 from codegen.modal import MODAL_WORKSPACE
 
-RUN_CODEMOD_ENDPOINT = f"https://{MODAL_WORKSPACE}--run-codemod-cli.modal.run"
+RUN_CODEMOD_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-run-codemod.modal.run"
