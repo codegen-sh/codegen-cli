a8d6c01df2384ab53c7b668ba9ee7791eb74c004
Christine Wang <christine@codegen.com>
2024-12-13 11:10:09 -0800

    fix safe modal execute + return early

--- src/codegen/run/process_output.py
+++ src/codegen/run/process_output.py
@@ -2,17 +2,20 @@ from requests import Response
 
 from codegen.api.schemas import RunCodemodOutput
 from codegen.diff.pretty_print import pretty_print_diff
-from codegen.utils.pydantic import safe_parse_json
+from codegen.utils.pydantic_utils import safe_parse_json
 
 
 def run_200_handler(payload: dict, response: Response):
     run_output = safe_parse_json(response.json(), RunCodemodOutput)
     if not run_output:
-        print(f"422 UnprocessableEntity: {response.text}")
+        print(f"422 UnprocessableEntity: {response.json()}")
+        return
     if not run_output.success:
         print(f"500 InternalServerError: {run_output.observation}")
+        return
     if payload.get("web"):
         # TODO: also open in the browser
         print(run_output.web_link)
-    else:
-        pretty_print_diff(run_output.logs)
+        return
+
+    pretty_print_diff(run_output.logs)
