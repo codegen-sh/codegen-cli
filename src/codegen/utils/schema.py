b46a08a33755a4498165e9bb5139e44726713e68
Christine Wang <christine@codegen.com>
2024-12-13 12:23:49 -0800

    chore: instead extend the `BaseModel` `model_validate` (#53)

--- src/codegen/utils/schema.py
+++ src/codegen/utils/schema.py
@@ -1,13 +1,11 @@
-from typing import TypeVar
-
 from pydantic import BaseModel
 
-T = TypeVar("T", bound=BaseModel)
 
-
-def safe_parse_json(json_obj: dict, model: type[T]) -> T | None:
-    try:
-        return model.model_validate(json_obj)
-    except Exception as e:
-        print(e)
-        return None
+class SafeBaseModel(BaseModel):
+    @classmethod
+    def model_validate(cls, data: dict) -> "SafeBaseModel":
+        try:
+            return super().model_validate(data)
+        except Exception as e:
+            print(e)
+            return None
