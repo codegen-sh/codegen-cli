a4907d4333dad7ffe69be22744092f132de2c7f5
Christine Wang <christine@codegen.com>
2024-12-13 14:55:23 -0800

    chore: update run to take in a repo path instead (#59)

--- src/codegen/utils/git/url.py
+++ src/codegen/utils/git/url.py
@@ -1,5 +1,20 @@
+from urllib.parse import urlparse
+
 from pygit2.repository import Repository
+
+
+def get_repo_full_name(repo: Repository) -> str:
+    return get_repo_full_name_from_url(get_git_url(repo))
 
 
 def get_git_url(repo: Repository) -> str:
     return repo.remotes[0].url
+
+
+def get_repo_full_name_from_url(git_url: str) -> str:
+    git_url = git_url.replace(".git", "")
+    url_parts = urlparse(git_url).path.strip("/").split("/")
+    if len(url_parts) < 2:
+        return None
+    repo_full_name = f"{url_parts[0]}/{url_parts[1]}"
+    return repo_full_name
