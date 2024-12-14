1c2ffcbec4657d0ff9156de7571b8a2ed254fcba
Christine Wang <christine@codegen.com>
2024-12-13 16:05:31 -0800

    chore: repo path optional use cwd by default (#62)

--- src/codegen/commands/run.py
+++ src/codegen/commands/run.py
@@ -18,17 +18,18 @@ from codegen.utils.git.url import get_repo_full_name
 @click.command(name="run")
 @track_command()
 @click.argument("codemod_path", required=True, type=click.Path(exists=True, path_type=Path))
-@click.argument("repo_path", required=True, type=click.Path(exists=True, path_type=Path))
+@click.argument("repo_path", required=False, type=click.Path(exists=True, path_type=Path))
 @click.option(
     "--web",
     is_flag=True,
     help="Return a web link to the diff",
 )
-def run_command(codemod_path: Path, repo_path: Path, web: bool = False):
+def run_command(codemod_path: Path, repo_path: Path | None = None, web: bool = False):
     """Run code transformation on the provided Python code.
 
     Arguments:
-        codemod_path: Path to the codemod file to execute
+        (required) codemod_path: Path to the codemod file to execute
+        (optional) repo_path: Path to the repository to run the codemod on. Defaults to the current working directory.
 
     """
     click.echo(f"Run codemod_path={codemod_path} repo_path={repo_path} ...")
@@ -38,19 +39,20 @@ def run_command(codemod_path: Path, repo_path: Path, web: bool = False):
     # if not auth_token:
     #     raise AuthError("Not authenticated. Please run 'codegen login' first.")
 
-    # TODO: also create an input schema
+    repo_path = repo_path or Path.cwd()
     git_repo = get_git_repo(repo_path)
     if not git_repo:
         click.echo(f"400 BadRequest: No git repository found at {repo_path}")
 
+    # TODO: also use a model for the input
     payload = {
         "repo_full_name": get_repo_full_name(git_repo),
         "codemod_source": codemod_path.read_text(),
         "web": web,
     }
 
-    print(f"Sending request to {RUN_CODEMOD_ENDPOINT} ...")
-    print(f"Payload: {json.dumps(payload, indent=4)}")
+    click.echo(f"Sending request to {RUN_CODEMOD_ENDPOINT} ...")
+    click.echo(f"Payload: {json.dumps(payload, indent=4)}")
 
     response = requests.post(
         RUN_CODEMOD_ENDPOINT,
