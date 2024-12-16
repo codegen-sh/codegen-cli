9ef6017b2654ad660e45e283c3dee4a4bcf0f918
eacodegen <eagarwal@codegen.com>
2024-12-16 13:32:53 -0800

    Use url parsing and set org/repo name (#85)
    Co-authored-by: eacodegen <eacodegen@users.noreply.github.com>

--- src/codegen/utils/git/url.py
+++ src/codegen/utils/git/url.py
@@ -1,24 +1,10 @@
-from urllib.parse import urlparse
-
+import giturlparse
 from pygit2.repository import Repository
 
 
-def get_repo_full_name(repo: Repository) -> str:
-    full_name = get_repo_full_name_from_url(get_git_url(repo))
-    if not full_name:
-        raise ValueError("Could not determine repository full name from URL")
-    full_name = full_name.split(":")[1]
-    return full_name
-
-
-def get_git_url(repo: Repository) -> str:
-    return repo.remotes[0].url
-
-
-def get_repo_full_name_from_url(git_url: str) -> str:
-    git_url = git_url.replace(".git", "")
-    url_parts = urlparse(git_url).path.strip("/").split("/")
-    if len(url_parts) < 2:
-        return None
-    repo_full_name = f"{url_parts[0]}/{url_parts[1]}"
-    return repo_full_name
+def get_git_organization_and_repo(repo: Repository) -> tuple[str, str]:
+    for remote in repo.remotes:
+        if remote.name == "origin":
+            parsed = giturlparse.parse(remote.url)
+            return parsed.owner, parsed.name
+    raise ValueError("No git remote found")
