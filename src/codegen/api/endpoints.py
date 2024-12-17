1315dcaa468fa011b03f824cfdfadd32415b9d7e
jayhack <jayhack.0@gmail.com>
2024-12-16 17:01:54 -0800

    .

--- src/codegen/api/endpoints.py
+++ src/codegen/api/endpoints.py
@@ -8,4 +8,4 @@ EXPERT_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-ask-expert.modal.run"
 LIST_DOCS_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-list-docs.modal.run"
 LIST_SKILLS_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-list-skills.modal.run"
 
-CREATE_ENDPOINT = "https://codegen-sh-staging--cli-create.modal.run"
+CREATE_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-create.modal.run"
