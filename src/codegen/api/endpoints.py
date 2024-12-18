ea2e20e50ac79700fe9de22ad6518f086a4fc7bd
Christine Wang <christine@codegen.com>
2024-12-18 11:22:38 -0800

    chore: update run endpoint `run-cli` -> `run` (#120)

--- src/codegen/api/endpoints.py
+++ src/codegen/api/endpoints.py
@@ -1,6 +1,6 @@
 from codegen.api.modal import MODAL_WORKSPACE
 
-RUN_CODEMOD_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-run-codemod.modal.run"
+RUN_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-run.modal.run"
 DOCS_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-docs.modal.run"
 EXPERT_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-ask-expert.modal.run"
 IDENTIFY_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-identify.modal.run"
