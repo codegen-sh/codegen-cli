4448bf6474dd2da77a44bc15e4392361f94c0a81
Ellen Agarwal <eagarwal@codegen.com>
2024-12-18 15:53:16 -0800

    use def execute

--- src/codegen/codemod/convert.py
+++ src/codegen/codemod/convert.py
@@ -1,3 +1,6 @@
+from textwrap import indent
+
+
 def convert_to_cli(input: str, language: str) -> str:
     codebase_type = "PyCodebaseType" if language.lower() == "python" else "TSCodebaseType"
     return f"""
@@ -6,12 +9,10 @@ from app.codemod.compilation.models.context import CodemodContext
 from app.codemod.compilation.models.pr_options import PROptions
 from graph_sitter import {codebase_type}
 
-codebase: {codebase_type}
-pr_options: PROptions
-context: CodemodContext
 
-# Write your code here:
-{input}
+def execute(codebase: {codebase_type}, pr_options: PROptions, context: CodemodContext):
+    # Write your code here:
+{indent(input, "    ")}
 """
 
 
