f859fce9b6b42ae0feb093266fcd384e545b7ef6
Ellen Agarwal <eagarwal@codegen.com>
2024-12-18 16:21:30 -0800

    Update convert script

--- src/codegen/codemod/convert.py
+++ src/codegen/codemod/convert.py
@@ -3,14 +3,16 @@ from textwrap import indent
 
 def convert_to_cli(input: str, language: str) -> str:
     codebase_type = "PyCodebaseType" if language.lower() == "python" else "TSCodebaseType"
-    return f"""
-# Type hints for injected variables
+    return f"""# Type hints for injected variables
 from app.codemod.compilation.models.context import CodemodContext
 from app.codemod.compilation.models.pr_options import PROptions
 from graph_sitter import {codebase_type}
 
+context: CodemodContext
 
-def execute(codebase: {codebase_type}, pr_options: PROptions, context: CodemodContext):
+
+def execute(codebase: {codebase_type}, pr_options: PROptions):
+    print = codebase.log
     # Write your code here:
 {indent(input, "    ")}
 """
